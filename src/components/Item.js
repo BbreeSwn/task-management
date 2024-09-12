19262b2ab8717ca00a582337d676362fefbbcf7b
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-12 16:29:49 +0700

    feat:add icon

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,5 @@
 import "./Item.css";
+import { FaRegTrashAlt , FaRegEdit  } from "react-icons/fa";
 
 export default function Item(props) {
   const { data, deleteTask,editTask } = props;
@@ -6,10 +7,9 @@ export default function Item(props) {
     <div className="list-item">
       <p className="title">{data.title}</p>
       <div className="button-container">
-        <button className="btn" onClick={() => deleteTask(data.id)}>
-          delete
-        </button>
-        <button className="btn" onClick={()=>editTask(data.id)}>edit</button>
+      <FaRegTrashAlt className="btn" onClick={() => deleteTask(data.id)}  />
+       <FaRegEdit className="btn" onClick={()=>editTask(data.id)} />
+       
       </div>
     </div>
   );
