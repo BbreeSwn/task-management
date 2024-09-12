0fbad7715b8e57324e84522feb148c6a947dd142
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-12 11:54:21 +0700

    feat:create state in App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
 import Item from "./components/Item";
+import {useState} from "react"
 
 function App() {
+  const [task , setTask] = useState([
+    {id:1,title:"fix bugs"},
+    {id:2,title:"study react"},
+    {id:3,title:"Cooking"}
+  ])
   return (
     <div className="App">
       <Header />
